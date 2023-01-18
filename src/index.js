a31195e3fe1f48a5e84e9e40bd8e4c0f57f8099d
Denis Ivanchenko <sicknature24@yandex.com>
2023-01-18 03:35:46 +0300

    fix: change to switch statement, code refactor

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 module.exports = function toReadable(number) {
-
-    let sNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
+    let smallNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
         'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let decaNumbers = ['twent', 'thirt', 'fort', 'fift', 'sixt', 'sevent', 'eight', 'ninet'];
 
-    let a = String(number).slice(1);
-
-    if (number < 20) {
-        return sNumbers[number];
-    } else if (number % 10 === 0 && number < 100) {
-        return decaNumbers[Math.floor(number / 10) - 2] + 'y';
-    } else if (!number % 10 === 0 && number < 100) {
-        return decaNumbers[Math.floor(number / 10) - 2] + 'y ' + sNumbers[number % 10];
-    } else if (number >= 100 && number % 100 === 0) {
-        return sNumbers[Math.floor(number / 100)] + " hundred";
-    } else if (number >= 100 && parseInt(a) < 20) {
-        let result = sNumbers[Math.floor(number / 100)] + " hundred " + sNumbers[a];
-        return result.trim();
-    } else if (a[0] === 0 && number >= 100) {
-        let b = a[1];
-        let result = sNumbers[Math.floor(number / 100)] + " hundred " + sNumbers[b];
-        return result.trim();
-    } else {
-        let result = sNumbers[Math.floor(number / 100)] + " hundred " + decaNumbers[Math.floor(String(number).slice(1, 2)) - 2] + 'y ' + sNumbers[number % 10].replace("zero", "");
-        return result.trim();
+    let numberString = number.toString();
+    switch (true) {
+        case (numberString < 20):
+            return smallNumbers[numberString];
+        case (numberString % 10 === 0 && numberString < 100):
+            return decaNumbers[Math.floor(numberString / 10) - 2] + 'y';
+        case (!numberString % 10 === 0 && numberString < 100):
+            return decaNumbers[Math.floor(numberString / 10) - 2] + 'y ' + smallNumbers[numberString % 10];
+        case (numberString >= 100 && numberString % 100 === 0):
+            return smallNumbers[Math.floor(numberString / 100)] + " hundred";
+        case (numberString >= 100 && numberString[1] === '0'):
+            return smallNumbers[Math.floor(numberString / 100)] + " hundred " + smallNumbers[numberString % 10];
+        case (numberString >= 100 && numberString.slice(1) < 20):
+            return smallNumbers[Math.floor(numberString / 100)] + " hundred " + smallNumbers[numberString.slice(1)];
+        case (numberString >= 100 && numberString.slice(1) < 100 && numberString % 10 === 0):
+            return smallNumbers[Math.floor(numberString / 100)] + " hundred " + decaNumbers[Math.floor(numberString.slice(1) / 10) - 2] + 'y';
+        case (numberString >= 100 && numberString.slice(1) < 100 && !numberString % 10 === 0):
+            return smallNumbers[Math.floor(numberString / 100)] + " hundred " + decaNumbers[Math.floor(numberString.slice(1) / 10) - 2] + 'y ' + smallNumbers[numberString % 10];
+        default:
+            return 'error';
     }
 }
